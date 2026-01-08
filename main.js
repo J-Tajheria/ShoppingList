9cc9081f9aa070ef9a5fec7e0c2ec5061526eb5c
Jordan <jordan.roberts@publicissapient.com>
2026-01-08 13:09:55 +0000

    add: Functions to create a new item and show the value onto the list

---
+++ main.js
@@ -0,0 +1,52 @@
+// Declare the elements that will be used into variables
+// Defined in the global scope so that they can be accessed by all functions
+
+const form = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const formButton = document.querySelector('form button');
+const filterInput = document.getElementById('filter');
+const itemList = document.getElementById('item-list');
+
+function addItem(e){
+    // Prevent form from submitting
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+
+    // validate input
+    if (newItem === ''){
+        alert('Please add an item')
+        return;
+    }
+
+    // create list item
+    const li  = document.createElement('li');
+    li.appendChild(document.createTextNode(newItem))
+    const button = createButton('remove-item btn-link text-red');
+
+    // Append button to the new item and append new item to existing list.
+    li.appendChild(button);
+    itemList.appendChild(li);
+
+    // Clear input
+    itemInput.value = '';
+}
+
+// Functin to create buttons
+function createButton(classes){
+    const btn = document.createElement('button');
+    btn.className = classes;
+
+    const icon = createIcon('fa-solid fa-xmark')
+    btn.appendChild(icon);
+    return btn
+}
+
+// Function to create icon and pass to the button
+function createIcon(classes){
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
+// Event Listeners
+form.addEventListener('submit', addItem);
